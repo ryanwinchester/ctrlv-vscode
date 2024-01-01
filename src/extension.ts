7eca2f55d6e7acb2d43661016ee9c41aace13558
Ryan Winchester <ryan@winchester.dev>
2023-12-31 22:01:33 -0400

    Make it work with fetch

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,77 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as https from 'https';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+type ReqHeaders = {
+  'Content-Type': string,
+  'Authorization'?: string
+};
+
+type DataSuccess = {
+  link: string,
+  puid: string,
+  expires_at: string,
+};
+
+type DataError = {
+  errors: string,
+};
+
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('ctrlv is now active!');
+  let disposable = vscode.commands.registerCommand('ctrlv.post', async () => {
+    const config = vscode.workspace.getConfiguration('ctrlv');
+    const editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ctrlv.post', () => {
-		// Get selected text.
-		// If none, get contents of current file.
-		// Check config for ctrlv API key.
-		// Post to ctrlv API.
-		// Get link from response.
-		// Put link into clipboard.
+    if (!editor) {
+      vscode.window.showWarningMessage('No open text editor to copy');
+      return;
+    }
 
-		// If it failed show a warning or error message.
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Link copied to clipboard!');
-	});
+    const language = editor.document.languageId;
+    const selection = editor.selection;
 
-	context.subscriptions.push(disposable);
+    const content = selection.isEmpty
+      ? editor.document.getText()
+      : editor.document.getText(selection);
+
+    try {
+      const host = config.get('host');
+      const apiKey = config.get('apiKey');
+      const expiresIn = config.get('expiresIn');
+
+      let headers = {
+        'Content-Type': 'application/json',
+      } as ReqHeaders;
+
+      if (apiKey) {
+        headers['Authorization'] = `Bearer ${apiKey}`;
+      }
+
+      const response = await fetch(`${host}/api/paste`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({
+          content: content,
+          language: language,
+          expires_in: expiresIn,
+        }),
+      });
+
+      if (response.status === 201) {
+        const data = await response.json() as DataSuccess;
+        await vscode.env.clipboard.writeText(data.link);
+        vscode.window.showInformationMessage('Link copied to clipboard');
+      } else {
+        const data = await response.json() as DataError;
+        vscode.window.showErrorMessage(
+          `Error posting code to ${host}: ${JSON.stringify(data.errors)}`
+        );
+      }
+    } catch (error) {
+      vscode.window.showErrorMessage(`Error: ${error}`);
+    }
+  });
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
